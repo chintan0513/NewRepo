4450365dae29f9c86b51de34d232661174f47085
shaily5 <shailyp05@gmail.com>
2024-01-05 21:29:05 -0500

    email service updated

---
+++ src/components/About.js
@@ -0,0 +1,39 @@
+import pic from '../assets/46.jpeg'
+import '../styles/about.css'
+import { BsInstagram, BsGithub, BsLinkedin } from 'react-icons/bs';
+import Skills from './Skills';
+
+const About = () => {
+    return (
+        <div className="about" id="about" >
+            <h1 className='h1'>About</h1>
+            <div className="card" id="card">
+                <div className="image">
+                    <img src={pic} alt="pic" className="my-pic" />
+                </div>
+                <hr />
+                <div className="content">
+                    <div className='info'>
+                        <p>Hi, there. I am Srushty Patel and I have completed my B.tech in SAL College OF Engineering in 2022. 1.5 years of industry experience in Java, Python, JavaScript, ReactJS, NodeJS, MongoDB, JIRA, Agile.Quick learner with a dynamic mindset, ready to adapt and contribute innovative ideas to drive success in a collaborative and forward-thinking environment.
+                            Seeking internship opportunities to apply extensive knowledge and gain valuable real-world experience.</p>
+                    </div>
+                    <div className="social-media">
+                        <div className='link-group'>
+                            <a href='https://www.instagram.com/shailyy5/' rel="noreferrer" target="_blank"><BsInstagram /></a>
+                        </div>
+                        <div className='link-group'>
+                            <a href='https://github.com/shaily5' rel="noreferrer" target="_blank"><BsGithub /></a>
+                        </div>
+                        <div className='link-group'>
+                            <a href='https://www.linkedin.com/in/shaily5/' rel="noreferrer" target="_blank"><BsLinkedin /></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <Skills />
+        </div>
+    );
+}
+
+export default About;
---
+++ src/projects.js
@@ -0,0 +1,98 @@
+import main from "./assets/Pottervrese.png";
+export const projects = [
+  {
+    _id: "1",
+    title: "Medical ChatBot",
+    typo: 'Full Stack',
+    content:
+      "Created a live medical chatbot using HTML, CSS, Bootstrap, Tailwind, and the MERN stack (MongoDB, Express, React, Node.js). This intelligent chatbot assists users in resolving their medical queries, provides suggestions regarding suitable medications for various health conditions.      ",
+    category: "MERN stack React",
+    image: "https://i.ibb.co/n1pT7f2/Chat-Bot-App.png",
+    link: "https://github.com/shaily5/MedicalChatbot"
+  },
+  {
+    _id: "2",
+    title: "Kalakari",
+    typo: "Front End",
+    content:
+      "Itchy eyes can be triggered by exposure to pollen, animal fur, mould, dust mites, make-up or eye drops.",
+    category: "Front End",
+    image: "https://i.ibb.co/VHWc4pk/Kalakari.png",
+    link: "https://shaily5.github.io/Kalakari/",
+  },
+  {
+    _id: "4",
+    title: "Pottervrese",
+    typo: 'Front End',
+    content:
+      "Itchy eyes can be triggered by exposure to pollen, animal fur, mould, dust mites, make-up or eye drops.",
+    category: "React.js, Javascript, TailwindCSS  ",
+    image: main,
+    link: "https://github.com/shaily5/Potterverse",
+  },
+  {
+    _id: "5",
+    title: "Music Recommender",
+    typo: 'Python',
+    content:
+      "Itchy eyes can be triggered by exposure to pollen, animal fur, mould, dust mites, make-up or eye drops.",
+    category: "React.js, Javascript, TailwindCSS  ",
+    image: "",
+    link: "https://github.com/shaily5/MusicRecommender",
+  },
+  {
+    _id: "6",
+    title: "Aspiring Minds",
+    typo: 'Full Stack',
+    content:
+      "A new Flutter project. ",
+    category: "React.js, Javascript, TailwindCSS  ",
+    image: "",
+    link: "https://github.com/shaily5/AspiringMinds/",
+  },
+  {
+    _id: "7",
+    title: "Global Search Engine",
+    typo: 'Java',
+    content:
+      "A search engine is a tool that helps users to find information from the web. For the user,the search engine returns the most accurate matches from the internet. The project covered concepts like web crawling, handling HTML files, website searching, frequency analysis, and pattern evaluation.",
+    category: "Java",
+    image: "Java",
+    link: "https://github.com/shaily5/GlobalSearchEngine",
+  },
+  {
+    _id: "8",
+    title: "Blogging App",
+    typo: 'Front End',
+    content:
+      "Created a Blog App using Node.js and Express.js with MongoDB that allows users to create, update, and delete blogs, as well as users add, update, and delete comments on those blogs.",
+    category: "React.js, Javascript, TailwindCSS  ",
+    image: "",
+    link: "https://github.com/shaily5/BloggingPlatform",
+  }
+];
+
+export default projects;
+
+export const buttons = [
+  {
+    name: "All",
+    value: "all"
+  },
+  {
+    name: "Front End",
+    value: "Front End"
+  },
+  {
+    name: "Full Stack",
+    value: "Full Stack"
+  },
+  {
+    name: "Java",
+    value: "Java"
+  },
+  {
+    name: "Python",
+    value: "Python"
+  }
+];
